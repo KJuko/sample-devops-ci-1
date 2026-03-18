8c38a65657ef14104451faf7ade0686f1b42f99c
KJuko <kalimsous9999@gmail.com>
2026-03-18 16:19:11 +0700

    feat: new feature

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import jest from "eslint-plugin-jest";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["tests/**/*.test.js"], plugins: { jest }, extends: ["plugin:jest/recommended"], languageOptions: { globals: globals.jest } },
 ]);
